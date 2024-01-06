0641d8d32090978450af87f0ad0888201df17590
KOPPOLA GOKUL SAI <koppolagokulsai@KOPPOLAs-MacBook-Air.local>
2024-01-06 20:24:49 +0530

    final snake ai

---
+++ RL_game.py
@@ -0,0 +1,173 @@
+import pygame
+from pygame.math import Vector2
+import random 
+import numpy as np
+
+#initialize pygame library
+pygame.init()
+
+
+
+#set no of blocks should be there in screen to form grid
+cell_size = 40
+cell_number = 10
+
+screen_size = cell_number * cell_size
+
+#define screen area for displaying game
+screen = pygame.display.set_mode((screen_size,screen_size))
+screen.fill((175,215,70)) # screen background color 
+
+#create timer to trigger change in display
+screen_update = pygame.USEREVENT
+pygame.time.set_timer(screen_update,10)
+
+clock = pygame.time.Clock()
+
+#font for displaying text on screen
+font = pygame.font.Font('arial.ttf', 25)
+
+# create a class for fruit 
+class FRUIT:
+    def __init__(self):
+        self.randomize()
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size),
+                                 int(self.pos.y * cell_size),
+                                 cell_size,
+                                 cell_size)
+        pygame.draw.rect(screen,(255,0,0),fruit_rect)
+    
+    def randomize(self):
+        self.x = random.randint(0,cell_number-1)
+        self.y = random.randint(0,cell_number-1)
+        self.pos = Vector2(self.x,self.y)
+
+#create a snake class
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(5,5),Vector2(4,5),Vector2(3,5)]
+        self.direction = Vector2(1,0) 
+        self.new_block = False
+    def draw_snake(self):
+        for block in self.body:
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos,
+                                     y_pos,
+                                     cell_size,
+                                     cell_size)
+            pygame.draw.rect(screen,(255,255,0),block_rect)
+    def move_snake(self,action):
+        clockwise = [Vector2(1,0),Vector2(0,1),Vector2(-1,0),Vector2(0,-1)]
+        idx = clockwise.index(self.direction)
+        if np.array_equal(action,[1,0,0]):
+            new_direction = clockwise[idx]
+        if np.array_equal(action,[0,1,0]):
+            next_idx = (idx+1)%4
+            new_direction = clockwise[next_idx]
+        if np.array_equal(action,[0,0,1]):
+            next_idx = (idx-1)%4
+            new_direction = clockwise[next_idx]
+
+        self.direction = new_direction
+
+        if self.new_block:
+            body_copy = self.body[:]
+            body_copy.insert(0,body_copy[0]+self.direction)
+            self.body = body_copy
+            self.new_block = False
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0,body_copy[0]+self.direction)
+            self.body = body_copy
+
+    def add_block(self):
+        self.new_block = True
+
+    def reset(self):
+        self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
+        self.direction = Vector2(1,0) 
+
+# create main class for handling all the logic
+class MAIN:
+    def __init__(self):
+        self.snake = SNAKE()
+        self.fruit = FRUIT()
+        self.score = 0
+        self.iteration = 0
+        self.game_ended = False
+        self.reward = 0
+    def update(self,action):
+        self.snake.move_snake(action)
+        self.check_collision()
+        self.check_fail()
+        # pygame.display.update()
+       
+
+    def draw_elements(self):
+        self.fruit.draw_fruit()
+        self.snake.draw_snake()
+        self.draw_score()
+
+    def draw_score(self):
+        text = font.render("Score: " + str(self.score), True, (255,255,255))
+        screen.blit(text,(0,0))
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]:
+            # print("collision")
+            self.fruit.randomize()
+            self.snake.add_block()
+            self.score += 1
+            self.reward += 10 
+
+    def check_fail(self, pt = None)->bool:
+        boolvalue = False
+        
+        if pt == None:
+            pt = self.snake.body[0]
+
+        if not (0 <= pt.x <= cell_number -1) or not (0 <= pt.y <= cell_number -1):
+            boolvalue  = True
+            
+        for block in self.snake.body[1:]:
+            if block == pt:
+                boolvalue  = True
+        return boolvalue
+    
+    def play(self,action):
+        self.iteration += 1
+        screen.fill((175,215,70))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.display.quit()
+                pygame.quit()
+                quit()
+            # move the snake 
+            if event.type == screen_update:
+                self.update(action)
+            
+
+        self.draw_elements()
+        pygame.display.update()
+        clock.tick(60)
+        if (self.check_fail() == True) or (self.iteration > 100*len(self.snake.body)):
+            self.game_ended = True
+            self.reward += -10
+            return self.reward, True, self.score
+        else:
+            return self.reward, False, self.score
+        
+        
+    def game_over(self):
+        self.score = 0
+        self.snake.reset()
+        self.iteration = 0
+
+    
+
+    
+    
+    
+   
---
+++ agent.py
@@ -0,0 +1,146 @@
+import torch
+import random
+import numpy as np
+from RL_game import MAIN
+from collections import deque
+from pygame.math import Vector2
+from model import Linear_QNet, QTrainer
+from helper import plot
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0 #randomness
+        self.gamma = 0.9 # discount rate between 0 and 1
+        self.memory = deque(maxlen = MAX_MEMORY) # when memory is full removes left 
+        #model,train
+        self.model = Linear_QNet(11,256,3)
+        self.trainer = QTrainer(self.model,LR,self.gamma)
+
+    def get_state(self,game):
+        head = game.snake.body[0]
+        point_l = Vector2(head.x - 40, head.y)
+        point_r = Vector2(head.x + 40, head.y)
+        point_u = Vector2(head.x, head.y - 40)
+        point_d = Vector2(head.x, head.y + 40)
+        
+        dir_l = game.snake.direction == Vector2(-1,0)
+        dir_r = game.snake.direction == Vector2(1,0)
+        dir_u = game.snake.direction == Vector2(0,-1)
+        dir_d = game.snake.direction == Vector2(0,1)
+
+        state = [
+            # Danger straight
+            (dir_r and game.check_fail(point_r)) or 
+            (dir_l and game.check_fail(point_l)) or 
+            (dir_u and game.check_fail(point_u)) or 
+            (dir_d and game.check_fail(point_d)),
+
+            # Danger right
+            (dir_u and game.check_fail(point_r)) or 
+            (dir_d and game.check_fail(point_l)) or 
+            (dir_l and game.check_fail(point_u)) or 
+            (dir_r and game.check_fail(point_d)),
+
+            # Danger left
+            (dir_d and game.check_fail(point_r)) or 
+            (dir_u and game.check_fail(point_l)) or 
+            (dir_r and game.check_fail(point_u)) or 
+            (dir_l and game.check_fail(point_d)),
+            
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+            
+            # Food location 
+            game.fruit.pos.x < game.fruit.pos.x,  # food left
+            game.fruit.pos.x > game.fruit.pos.x,  # food right
+            game.fruit.pos.y < game.fruit.pos.y,  # food up
+            game.fruit.pos.y > game.fruit.pos.y  # food down
+            ]
+        
+        return np.array(state, dtype=int)
+    
+    def remember(self,state,action,reward,next_state,done):
+        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
+    
+    def train_long_memory(self):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+
+    def train_short_memory(self,state,action,reward,next_state,done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+    def get_action(self,state):
+        # first we go with random moves to understand about evironment
+        self.epsilon = 80 - self.n_games
+        final_move = [0,0,0]
+        if random.randint(0,200)<self.epsilon:
+            move = random.randint(0,2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state,dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
+
+
+def train():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = MAIN()
+
+    while True:
+        #get old state
+        state_old = agent.get_state(game)
+
+        #get move
+        final_move = agent.get_action(state_old)
+
+        #perform move and new state 
+
+        reward,done,score = game.play(final_move)
+
+        state_new = agent.get_state(game)
+
+        #train short memory
+
+        agent.train_short_memory(state_old,final_move,reward,state_new,done)
+
+        # remember 
+        agent.remember(state_old,final_move,reward,state_new,done)
+
+        if done:
+            #train long memory , plot result
+            game.game_over()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                # agnent.model.save
+            
+            print(f"game{agent.n_games}, score {score}, record {record}")
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
+
+train()
